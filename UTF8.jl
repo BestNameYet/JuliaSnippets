3c3f86be7fb5957e4fbe6acfcff989d216317090
BestNameYet <samuel.john.nixon@gmail.com>
2021-05-06 18:47:39 -0500

    Update UTF8.jl

--- UTF8.jl
+++ UTF8.jl
@@ -1,1 +1,16 @@
-module UTF8 end
+module UTF8
+
+function PrintableASCIItable()
+  span = 20:1:127
+  table = [span]
+  utf8table = broadcast(x -> string(x, base=16), table)
+  asciitable = broadcast(x -> Char(x), table)
+  return f(key, default) -> get(Dict{AbstractString, AbstractString}(table), key, default)
+end
+
+
+function validASCII end
+validASCII() = 
+
+
+end
