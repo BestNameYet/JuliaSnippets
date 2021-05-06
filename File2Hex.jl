db30fb88b2a0a86b4906cefadf64996990fe2595
BestNameYet <samuel.john.nixon@gmail.com>
2021-05-06 12:28:49 -0500

    Update File2Hex.jl

--- File2Hex.jl
+++ File2Hex.jl
@@ -3,13 +3,11 @@ module File2Hex
 function arrayfactory end
 arrayfactory() = io -> Array{UInt8, 1}(undef, stat(io).size)
 
-
 function readerfactory end
 readerfactory() = (arr = arrayfactory(); io -> read!(io, arr(io)))
 
 function openerfactory end
 openerfactory() = (reader = readerfactory(); fn -> open(reader, fn))
-
 
 function hexerfactory end
 hexerfactory() = (opener = openerfactory(); fn -> bytes2hex(opener(fn)))
