84c7fb379a702e5631a58a121a18db5c9e504eba
BestNameYet <samuel.john.nixon@gmail.com>
2021-05-06 12:28:27 -0500

    Update File2Hex.jl

--- File2Hex.jl
+++ File2Hex.jl
@@ -3,16 +3,21 @@ module File2Hex
 function arrayfactory end
 arrayfactory() = io -> Array{UInt8, 1}(undef, stat(io).size)
 
-function readerfactory() end
+
+function readerfactory end
 readerfactory() = (arr = arrayfactory(); io -> read!(io, arr(io)))
 
-function openerfactory() end
+function openerfactory end
 openerfactory() = (reader = readerfactory(); fn -> open(reader, fn))
 
-function hexerfactory() end
+
+function hexerfactory end
 hexerfactory() = (opener = openerfactory(); fn -> bytes2hex(opener(fn)))
 
-function file2hex(filename::AbstractString) end
+function file2hex end
 file2hex(filename::AbstractString) = (hexer = hexerfactory(); hexer(filename))
+
+
+y = file2hex("test.txt")
 
 end
