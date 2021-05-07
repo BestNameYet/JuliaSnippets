53ae7a4fb82b0c0987acc687e68da5e75d2686b2
BestNameYet <samuel.john.nixon@gmail.com>
2021-05-06 21:32:31 -0500

    Update UTF8.jl

--- UTF8.jl
+++ UTF8.jl
@@ -1,13 +1,14 @@
 module UTF8
 
 function hex2print()
-  table = [1:127;]
-  ascii = broadcast(x -> Char(x), table)
-  printable_ascii = filter(isprint, ascii)
-  printable_utf8_integers = broadcast(x -> codepoint(x), printable_ascii)
-  printable_utf8_strings = broadcast(x -> string(x, base=16, pad=2), printable_utf8_integers)
-  printable_utf8_ascii = broadcast((utf8, ascii)->(utf8, ascii), printable_utf8_strings, printable_ascii)
-  return (key) -> get(Dict{AbstractString, Char}(printable_utf8_ascii), key, Char(0))
+  [1:128;]
+  broadcast(x -> Char(x), ans)
+  printables = filter(isprint, ans)
+  broadcast(x -> codepoint(x), ans)
+  broadcast(x -> string(x, base=16, pad=2), ans)
+  broadcast((u, a)->(u, a), ans, printables)
+  Dict(ans)
+  return (key) -> get(ans, key, Char(0))
 end
 
 end
