8cc78915bdfc5ebc79ffb8b313180d5abf5e02a2
BestNameYet <samuel.john.nixon@gmail.com>
2021-05-06 19:21:31 -0500

    Update UTF8.jl

--- UTF8.jl
+++ UTF8.jl
@@ -5,7 +5,7 @@ function PrintableASCIItable()
   utf8table = broadcast(x -> string(x, base=16, pad=2), table)
   asciitable = broadcast(x -> Char(x), table)
   utf8ascii = broadcast((x, y) -> (x, y), utf8table, asciitable)
-  return f(key, default) -> get(Dict{AbstractString, AbstractChar}(utf8ascii), key, default)
+  return (key, default) -> get(Dict{AbstractString, AbstractChar}(utf8ascii), key, default)
 end
 
 end
