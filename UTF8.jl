47d69bad56fa435f80418f2ea330ddfca20eba83
BestNameYet <samuel.john.nixon@gmail.com>
2021-05-07 03:34:01 -0500

    Update UTF8.jl

--- UTF8.jl
+++ UTF8.jl
@@ -1,14 +1,10 @@
 module UTF8
 
-function hex2print()
-  [1:128;]
-  broadcast(x -> Char(x), ans)
-  printables = filter(isprint, ans)
-  broadcast(x -> codepoint(x), ans)
-  broadcast(x -> string(x, base=16, pad=2), ans)
-  broadcast((u, a)->(u, a), ans, printables)
-  Dict(ans)
-  return (key) -> get(ans, key, Char(0))
+function asciidict()
+    ascii = [1:128;] |> y -> broadcast(x -> Char(x), y) |> y -> filter(isprint, y)
+    dict = ascii |> y -> broadcast(x -> (codepoint(x), x), y) |>
+    y -> broadcast(x -> (x).|> (x -> string(x, base=16, pad=2), identity), y) |> Dict
+    return k -> get(dict, k, (Char(0)))
 end
 
 end
