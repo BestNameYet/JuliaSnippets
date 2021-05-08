780e49e8deb924c8f8eada5bc1d714d6194a5344
BestNameYet <samuel.john.nixon@gmail.com>
2021-05-08 01:14:15 -0500

    Update UTF8.jl

--- UTF8.jl
+++ UTF8.jl
@@ -1,15 +1,23 @@
 module UTF8
-
     
- function testhex(key)
+export testhex
+
+const printables = let
+    [1:128;] |>
+    (function(x);Char.(x);end;)|>
+    (function(x);filter(isprint,x);end)
+end
+
+const lookuptable = let f, g, h, pairs, dict
     f(x) = string(x, base=16, pad=2)
-    g(x) = codepoint(x)
-    h(x) = tuple(f(g(x)), x)
-    ascii = [1:128;] |> (function(x);Char.(x);end;)|>(function(x);filter(isprint,x);end)
-    pairs = h.(ascii)
+    g(x) = codepoint(x); h(x) = tuple(f(g(x)), x)
+    pairs = h.(printables)
     dict = Dict(pairs)
-    get(dict, key, Char(0))
-end 
+end
+
+function testhex(key)
+    get(lookuptable, key, Char(0))
+end
 
 
 end
