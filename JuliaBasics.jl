374ba0af15dbf8849482cc4e2da2784e93388c28
BestNameYet <samuel.john.nixon@gmail.com>
2021-05-11 07:03:52 -0500

    Update JuliaBasics.jl

--- JuliaBasics.jl
+++ JuliaBasics.jl
@@ -1,5 +1,17 @@
 module JuliaBasics
 
+#-- Assignment statement
+#-- LHS: a name variable is a named location in memory.
+#-- RHS: a value (container, function, float, integer, object, string, etc.)
+
+a = 1;
+b = 1.0;
+c = "Hello World!"
+d = [a, b, c]
+e = (add1to(me) = me + 1)
+f = (struct MyStuff; this; that; thistoo; end)(a, b, c)
+println("$f.this, $f.that, $f.thistoo");
+
 
 
 end
