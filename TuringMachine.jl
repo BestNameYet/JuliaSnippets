a70908044897c20d1abf4d504d107f82c6082dd3
BestNameYet <samuel.john.nixon@gmail.com>
2021-05-11 14:21:34 -0500

    Update TuringMachine.jl

--- TuringMachine.jl
+++ TuringMachine.jl
@@ -1,44 +1,61 @@
 module TuringMachine.jl
 
-tape = [0,]
-address = 0
+using Random
+tape = rand(0:1,150)
+address = 30
 
 function A(tape)
+    if address < 1 || address > length(tape)
+        return "halt"
+    end
+    
   #(0, A, 1, B)
   if tape[address] == 0
     tape[address] = 1
-    address += 1
+    global address += 1
     return B
   #(1, A, 2, A)
   elseif tape[address] == 1
     tape[address] = 2
-    address += 1
+    global address += 1
     return A
    #(2, A, 1, A)
    elseif tape[address] == 2
     tape[address] = 1
-    address += 1
+    global address += 1
     return A
    end
 end
 
 function B(tape)
+    if address < 1 || address > length(tape)
+        return "halt"
+    end
   #(0, B, 2, A)
   if tape[address] == 0
     tape[address] = 2
-    address += 1
+    global address += 1
     return A
   #(1, B, 2, B)
   elseif tape[address] == 1
     tape[address] = 2
-    address += 1
+    global address += 1
     return B
    #(2, B, 0, A)
    elseif tape[address] == 2
     tape[address] = 0
-    address += 1
+    global address += 1
     return A
    end
 end
+
+x = A
+println("I: $tape")
+
+while x != "halt"
+    global x = x(tape)
+end
+
+println("H: $tape")
 
 end
