996a15f29a328f590888246c99440d416e305577
BestNameYet <samuel.john.nixon@gmail.com>
2021-05-12 16:59:26 -0500

    Update Parser.jl

--- Parser.jl
+++ Parser.jl
@@ -11,11 +11,27 @@ function findtoken(bits::Array{Uint8})
   end
 end
 
+function findstring(bits::Array{Uint8})
+  tokens::String = []
+  for (i, bit) in enumerate(bits)
+    if isascii(bit)
+      token = scanfortoken(i, bits)
+      !isnothing(token) && continue
+      tokens.push!(token)
+    end
+  end
+end
+
 function scanfortoken(index::Int, bits::AbstractArray)
   maybeascii = bits[index, index+Int(bits[index])]
   return isascii(maybestring) ? String(maybeascii) : nothing
 end
 
+function scanfortoken(index::Int, bits::AbstractArray)
+  while Char(i)
+  maybeascii = bits[index, index+Int(bits[index])]
+  return isascii(maybestring) ? String(maybeascii) : nothing
+end
 
 
 
