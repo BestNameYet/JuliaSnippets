5c3582135b153b9f5c82872532e12fcc51ed8b7b
BestNameYet <samuel.john.nixon@gmail.com>
2021-06-03 02:32:19 -0500

    Add files via upload

--- ReliefCapacities/src/liquids.jl
+++ ReliefCapacities/src/liquids.jl
@@ -1,9 +1,9 @@
 function flowcoeffecientLiquid(;inletPressure::typeof(1.0u"psi"), outletPressure::typeof(1.0u"psi"), specificGravity::typeof(1.0), volumetricflowrate::typeof(1.0u"gpm"))::typeof(1.0u"gpm*(1/sqrt(psi))")
     Q = volumetricflowrate;
     SG = specificGravity;
-    if(SG ≤ 0 || Q ≤ 0u"gpm") return 0u"gpm*(1/sqrt(psi))" end
+    if(SG ≤ 0.0 || Q ≤ 0.0u"gpm") return 0.0u"gpm*(1/sqrt(psi))" end
     ∆P = inletPressure - outletPressure;
-    if(∆P ≤ 0u"psi") return (1/0)u"gpm*(1/sqrt(psi))" end
+    if(∆P ≤ 0.0u"psi") return (1/0)u"gpm*(1/sqrt(psi))" end
     radicand = SG/∆P; 
     Cv = Q*sqrt(radicand)
     return Cv
@@ -14,7 +14,7 @@ function flowrateLiquid(;inletPressure::typeof(1.0u"psi"), outletPressure::typeo
     if(SG ≤ 0.0) return (1/0)u"gpm" end
     Cv = flowcoeffecient
     ∆P = inletPressure - outletPressure;
-    if(∆P ≤ 0.0u"psi" || Cv ≤ 0.0u"gpm*(1/sqrt(psi))") return 0u"gpm" end
+    if(∆P ≤ 0.0u"psi" || Cv ≤ 0.0u"gpm*(1/sqrt(psi))") return 0.0u"gpm" end
     radicand = ∆P/SG;
     Q = Cv*sqrt(radicand)
     return Q;
