f541e63ec69bbffb586af9addb58d30ab155d1e7
BestNameYet <samuel.john.nixon@gmail.com>
2021-06-03 00:03:33 -0500

    Add files via upload

--- Formulas.jl
+++ Formulas.jl
@@ -19,10 +19,10 @@ end
 
 function flowrateLiquid(;inletPressure::typeof(1.0u"psi"), outletPressure::typeof(1.0u"psi"), specificGravity::typeof(1.0), flowcoeffecient::typeof(1.0u"gpm*(1/sqrt(psi))"))::typeof(1.0u"gpm")
     SG = specificGravity;
-    if(SG ≤ 0) return (1/0)u"gpm" end
+    if(SG ≤ 0.0) return (1/0)u"gpm" end
     Cv = flowcoeffecient
     ∆P = inletPressure - outletPressure;
-    if(∆P ≤ 0u"psi" || Cv ≤ 0u"gpm*(1/sqrt(psi))") return 0u"gpm" end
+    if(∆P ≤ 0.0u"psi" || Cv ≤ 0.0u"gpm*(1/sqrt(psi))") return 0u"gpm" end
     radicand = ∆P/SG;
     Q = Cv*sqrt(radicand)
     return Q;
@@ -39,8 +39,10 @@ function criticalflowcoeffecient(;inletPressure::typeof(1.0u"psi"), inletTempera
     scalar = scalingfactor
     Q = volumetricflowrate
     T0 = uconvert(u"Ra",inletTemperature)
-    P0 = inletPressure
     SG = specificGravity
+    if(Q ≤ 0.0u"scfh" || SG ≤ 0.0 ||T0 ≤ 0.0u"Ra")  return 0.0u"sqrt(Ra)*scfh*(1/psi)" end
+    P0 = inletPressure
+    if(P0 ≤ 0.0u"psi") return (1/0)u"sqrt(Ra)*scfh*(1/psi)" end
     radicand = SG*T0
     upper = sqrt(radicand)
     lower = scalar*P0
@@ -52,9 +54,12 @@ function subcriticalflowcoeffecient(;inletPressure::typeof(1.0u"psi"), outletPre
     scalar = scalingfactor
     Q = volumetricflowrate
     T0 = uconvert(u"Ra",inletTemperature)
+    SG = specificGravity
+    if(Q ≤ 0.0u"scfh" || SG ≤ 0.0 ||T0 ≤ 0.0u"Ra")  return 0.0u"sqrt(Ra)*scfh*(1/psi)" end
     P0 = inletPressure
     P1 = outletPressure
-    SG = specificGravity
+    ∆P = P0 - P1
+    if(∆P ≤ 0.0u"psi") return (1/0)u"sqrt(Ra)*scfh*(1/psi)" end
     upper = SG*T0
     lower = (^(P0,2)-^(P1,2))
     radicand = upper/lower
