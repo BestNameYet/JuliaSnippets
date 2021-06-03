636d2221bc63dd7a06d54df93a9a41fbe95e695f
BestNameYet <samuel.john.nixon@gmail.com>
2021-06-03 02:42:18 -0500

    Add files via upload

---
+++ CvFlow.jl/src/CvFlow.jl
@@ -0,0 +1,14 @@
+module CvFlow
+
+using Unitful
+
+include("moreunits.jl")
+include("liquids.jl")
+include("gases.jl")
+include("tests.jl")
+
+export flowcoeffecientGas
+export flowcoeffecientLiquid, flowrateLiquid 
+
+
+end
